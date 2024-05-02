9c17da1239b5e399f1375c9ce72faeb4c29381c9
Papp Dániel <pappdani2@gmail.com>
2024-05-02 16:56:09 +0200

    login/register routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,14 +3,21 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home'
 import Video from './Pages/Video';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Header from './Components/Header';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token') !==  null ? true : false);
 
   return (
     <BrowserRouter>
+    <Header setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="video/:videoId" element={<Video />}/>
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
