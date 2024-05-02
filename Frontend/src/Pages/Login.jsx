a40f4814d3987f2cd007e7d4e7d43704e16f7620
Papp Dániel <pappdani2@gmail.com>
2024-05-02 16:55:24 +0200

    Login page implemented

---
+++ Frontend/src/Pages/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import'../css/Login.css'
+
+
+const Login = ({ setIsLoggedIn }) => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch(`http://localhost:5048/Auth/Login`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify( { email, password } )
+            })
+            if(response.ok) {
+                const data = await response.json();
+                const token = data.token;
+                localStorage.setItem('token', token);
+                setIsLoggedIn(true)
+                navigate("/")
+            } else {
+                const data = await response.json();
+                window.alert(data['Bad credentials'][0])
+            }
+        } catch(err) {
+            console.error(err)
+        }
+    }
+
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email: </label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="login-input"
+                    placeholder="Write email here"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <label htmlFor="password">Password: </label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    className="login-input"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit" className="login-btn">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Login
