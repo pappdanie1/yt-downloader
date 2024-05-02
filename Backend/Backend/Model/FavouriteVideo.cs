6b82c3cb2edd47689aab59d9f50e9f3a174b8753
Papp Dániel <pappdani2@gmail.com>
2024-05-02 13:23:03 +0200

    FavouriteVideo.cs model added

---
+++ Backend/Backend/Model/FavouriteVideo.cs
@@ -0,0 +1,13 @@
+using Backend.Data;
+
+namespace Backend.Model;
+
+public class FavouriteVideo
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string Title { get; set; }
+    public string Image { get; set; }
+    public string Url { get; set; }
+    public ApplicationUser User { get; set; }
+}
